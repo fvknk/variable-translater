506c4d74557ac32c73f17ed9f6e68122a7b7515a
fvknk <97640891+fvknk@users.noreply.github.com>
2024-11-04 17:23:02 +0900

    エラーメッセージを表示するように変更

--- src/extension.ts
+++ src/extension.ts
@@ -4,14 +4,18 @@ import { Runner } from './runner'
 
 export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('variable-translator.translateVariable', async () => {
-		const activeEditor = vscode.window.activeTextEditor
-		const doc = activeEditor && activeEditor.document
-		const ref = activeEditor?.selection
+		try {
+			const activeEditor = vscode.window.activeTextEditor
+			const doc = activeEditor && activeEditor.document
+			const ref = activeEditor?.selection
 
-		const text: string | undefined = doc?.getText(ref)
-		const message = await new Runner(text).exec()
+			const text: string | undefined = doc?.getText(ref)
+			const message = await new Runner(text).exec()
 
-		vscode.window.showInformationMessage(message)
+			vscode.window.showInformationMessage(message)
+		} catch (err: any) {
+			vscode.window.showErrorMessage(err.message)
+		}
 	})
 
 	context.subscriptions.push(disposable)
