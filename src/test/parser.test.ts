f81a7a3ded333c352d3577a5b98ca71ac782cd56
fvknk <97640891+fvknk@users.noreply.github.com>
2024-11-06 21:36:45 +0900

    クラスごとのテストをクラス名の describe で囲むように変更

--- src/test/parser.test.ts
+++ src/test/parser.test.ts
@@ -5,74 +5,76 @@ import { describe, it } from 'mocha'
 import { Parser } from '../parser'
 import { ValidationError } from '../error'
 
-describe('#constructor', () => {
-  describe('引数が空文字列でない場合', () => {
-    it('値が正しく代入されていること', () => {
-      const inputText = 'test'
-      const runner = new Parser(inputText)
+describe('Parse', () => {
+  describe('#constructor', () => {
+    describe('引数が空文字列でない場合', () => {
+      it('値が正しく代入されていること', () => {
+        const inputText = 'test'
+        const runner = new Parser(inputText)
 
-      assert.strictEqual(runner.inputText, inputText)
-      assert.strictEqual(runner.outputText, '')
+        assert.strictEqual(runner.inputText, inputText)
+        assert.strictEqual(runner.outputText, '')
+      })
+    })
+
+    describe('引数が空文字列の場合', () => {
+      it('エラーを返却すること', () => {
+        const inputText = ''
+
+        assert.throws(() => new Parser(inputText), ValidationError)
+      })
     })
   })
 
-  describe('引数が空文字列の場合', () => {
-    it('エラーを返却すること', () => {
-      const inputText = ''
+  describe('#exec', () => {
+    describe('正常に処理できる場合', () => {
+      describe('1ワードのみの場合', () => {
+        const inputText = 'test'
+        it('そのまま返却されること', () => {
+          const actual = new Parser(inputText).exec()
 
-      assert.throws(() => new Parser(inputText), ValidationError)
-    })
-  })
-})
-
-describe('#exec', () => {
-  describe('正常に処理できる場合', () => {
-    describe('1ワードのみの場合', () => {
-      const inputText = 'test'
-      it('そのまま返却されること', () => {
-        const actual = new Parser(inputText).exec()
-
-        assert.strictEqual(actual, inputText)
+          assert.strictEqual(actual, inputText)
+        })
       })
-    })
 
-    describe('スネークケースの場合', () => {
-      const inputText = 'foo_bar_baz'
-      it('空白に変換して返却すること', () => {
-        const expect = 'foo bar baz'
-        const actual = new Parser(inputText).exec()
+      describe('スネークケースの場合', () => {
+        const inputText = 'foo_bar_baz'
+        it('空白に変換して返却すること', () => {
+          const expect = 'foo bar baz'
+          const actual = new Parser(inputText).exec()
 
-        assert.strictEqual(actual, expect)
+          assert.strictEqual(actual, expect)
+        })
       })
-    })
 
-    describe('ケバブケースの場合', () => {
-      const inputText = 'foo-bar-baz'
-      it('空白に変換して返却すること', () => {
-        const expect = 'foo bar baz'
-        const actual = new Parser(inputText).exec()
+      describe('ケバブケースの場合', () => {
+        const inputText = 'foo-bar-baz'
+        it('空白に変換して返却すること', () => {
+          const expect = 'foo bar baz'
+          const actual = new Parser(inputText).exec()
 
-        assert.strictEqual(actual, expect)
+          assert.strictEqual(actual, expect)
+        })
       })
-    })
 
-    describe('キャメルケースの場合', () => {
-      const inputText = 'fooBarBaz'
-      it('空白を入れ、かつ小文字で返却すること', () => {
-        const expect = 'foo bar baz'
-        const actual = new Parser(inputText).exec()
+      describe('キャメルケースの場合', () => {
+        const inputText = 'fooBarBaz'
+        it('空白を入れ、かつ小文字で返却すること', () => {
+          const expect = 'foo bar baz'
+          const actual = new Parser(inputText).exec()
 
-        assert.strictEqual(actual, expect)
+          assert.strictEqual(actual, expect)
+        })
       })
-    })
 
-    describe('パスカルケースの場合', () => {
-      const inputText = 'FooBarBaz'
-      it('空白を入れ、かつ小文字で返却すること', () => {
-        const expect = 'foo bar baz'
-        const actual = new Parser(inputText).exec()
+      describe('パスカルケースの場合', () => {
+        const inputText = 'FooBarBaz'
+        it('空白を入れ、かつ小文字で返却すること', () => {
+          const expect = 'foo bar baz'
+          const actual = new Parser(inputText).exec()
 
-        assert.strictEqual(actual, expect)
+          assert.strictEqual(actual, expect)
+        })
       })
     })
   })
--- src/test/translator.test.ts
+++ src/test/translator.test.ts
@@ -6,92 +6,94 @@ import sinon from 'sinon'
 import { Translator } from '../translator'
 import { EmptyVariableError, ValidationError } from '../error'
 
-describe('#constructor', () => {
-  describe('空文字列でない場合', () => {
-    it('値が正しく代入されていること', () => {
-      const inputText = 'abcXYZ!?'
-      const translator = new Translator(inputText)
+describe('Translator', () => {
+  describe('#constructor', () => {
+    describe('空文字列でない場合', () => {
+      it('値が正しく代入されていること', () => {
+        const inputText = 'abcXYZ!?'
+        const translator = new Translator(inputText)
 
-      assert.strictEqual(translator.inputText, inputText)
-      assert.strictEqual(translator.response, null)
-      assert.strictEqual(translator.outputText, null)
-    })
-  })
-
-  describe('空文字列の場合', () => {
-    it('エラーを返却すること', () => {
-      const inputText = ''
-
-      assert.throws(() => new Translator(inputText), ValidationError)
-    })
-  })
-})
-
-describe('#exec', () => {
-  let requestStub: sinon.SinonStub
-  before(() => {
-    requestStub = sinon.stub(Translator.prototype, 'request')
-  })
-
-  after(() => {
-    requestStub.restore()
-  })
-
-  describe('リクエストが成功した場合', () => {
-    const res = { text: 'テスト' }
-    before(() => {
-      requestStub.resolves(res)
-    })
-
-    it('結果用文字列を返却すること', async () => {
-      const actual = await new Translator('test').exec()
-      assert.strictEqual(actual, res.text)
-    })
-
-    it('getter で返却値を取得できること', async () => {
-      const translator = new Translator('test')
-      await translator.exec()
-
-      assert.deepStrictEqual(translator.response, res)
-      assert.strictEqual(translator.outputText, res.text)
-    })
-  })
-
-  describe('リクエストが失敗した場合', () => {
-    describe('外部 API へのリクエストが失敗した場合', () => {
-      it('エラーを返却すること', () => {
-        requestStub.rejects()
-
-        assert.rejects(async () => await new Translator('test').exec(), EmptyVariableError)
+        assert.strictEqual(translator.inputText, inputText)
+        assert.strictEqual(translator.response, null)
+        assert.strictEqual(translator.outputText, null)
       })
     })
 
-    describe('レスポンスが不正な場合', () => {
-      describe('レスポンスが null の場合', () => {
+    describe('空文字列の場合', () => {
+      it('エラーを返却すること', () => {
+        const inputText = ''
+
+        assert.throws(() => new Translator(inputText), ValidationError)
+      })
+    })
+  })
+
+  describe('#exec', () => {
+    let requestStub: sinon.SinonStub
+    before(() => {
+      requestStub = sinon.stub(Translator.prototype, 'request')
+    })
+
+    after(() => {
+      requestStub.restore()
+    })
+
+    describe('リクエストが成功した場合', () => {
+      const res = { text: 'テスト' }
+      before(() => {
+        requestStub.resolves(res)
+      })
+
+      it('結果用文字列を返却すること', async () => {
+        const actual = await new Translator('test').exec()
+        assert.strictEqual(actual, res.text)
+      })
+
+      it('getter で返却値を取得できること', async () => {
+        const translator = new Translator('test')
+        await translator.exec()
+
+        assert.deepStrictEqual(translator.response, res)
+        assert.strictEqual(translator.outputText, res.text)
+      })
+    })
+
+    describe('リクエストが失敗した場合', () => {
+      describe('外部 API へのリクエストが失敗した場合', () => {
         it('エラーを返却すること', () => {
-          const res = null
-          requestStub.resolves(res)
+          requestStub.rejects()
 
           assert.rejects(async () => await new Translator('test').exec(), EmptyVariableError)
         })
       })
-    })
 
-    describe('レスポンス内にキーがない場合', () => {
-      it('エラーを返却すること', () => {
-        const res = {}
-        requestStub.resolves(res)
+      describe('レスポンスが不正な場合', () => {
+        describe('レスポンスが null の場合', () => {
+          it('エラーを返却すること', () => {
+            const res = null
+            requestStub.resolves(res)
 
-        assert.rejects(async () => await new Translator('test').exec(), EmptyVariableError)
-      })
-    })
+            assert.rejects(async () => await new Translator('test').exec(), EmptyVariableError)
+          })
+        })
 
-    describe('レスポンス内の結果が空文字列の場合', () => {
-      it('エラーを返却すること', () => {
-        const res = { text: '' }
-        requestStub.resolves(res)
+        describe('レスポンス内にキーがない場合', () => {
+          it('エラーを返却すること', () => {
+            const res = {}
+            requestStub.resolves(res)
 
-        assert.rejects(async () => await new Translator('test').exec(), EmptyVariableError)
+            assert.rejects(async () => await new Translator('test').exec(), EmptyVariableError)
+          })
+        })
+
+        describe('レスポンス内の結果が空文字列の場合', () => {
+          it('エラーを返却すること', () => {
+            const res = { text: '' }
+            requestStub.resolves(res)
+
+            assert.rejects(async () => await new Translator('test').exec(), EmptyVariableError)
+          })
+        })
       })
     })
   })
