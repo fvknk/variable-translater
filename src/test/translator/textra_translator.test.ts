5fe9f0797b89b005d53ac1e194053faf6e89a291
fvknk <97640891+fvknk@users.noreply.github.com>
2024-11-20 22:28:26 +0900

    TexTra 用のクラスを設定

---
+++ src/test/translator/textra_translator.test.ts
@@ -0,0 +1,106 @@
+import * as assert from 'assert'
+
+import { describe, it, before, after } from 'mocha'
+import sinon from 'sinon'
+
+import { TextraTranslator } from '../../translators/textra_translator'
+import { EmptyVariableError } from '../../error'
+
+describe('TextraTranslator', () => {
+  describe('#request', () => {
+    let apiKeyStub: sinon.SinonStub
+    let apiSecretStub: sinon.SinonStub
+    let fetchTokenStub: sinon.SinonStub
+    let fetchStub: sinon.SinonStub
+    before(() => {
+      apiKeyStub = sinon.stub(TextraTranslator.prototype, <any>'apiKey')
+      apiSecretStub = sinon.stub(TextraTranslator.prototype, <any>'apiSecret')
+      fetchTokenStub = sinon.stub(TextraTranslator.prototype, <any>'fetchToken')
+      fetchStub = sinon.stub(TextraTranslator.prototype, <any>'fetch')
+    })
+
+    after(() => {
+      apiKeyStub.restore()
+      apiSecretStub.restore()
+      fetchTokenStub.restore()
+      fetchStub.restore()
+    })
+
+    describe('リクエストが成功した場合', () => {
+      it('結果用文字列を返却すること', async () => {
+        const res = { text: 'テスト' }
+
+        apiKeyStub.returns('api_key')
+        apiSecretStub.returns('api_secret')
+        fetchTokenStub.resolves('token')
+        fetchStub.resolves(res.text)
+
+        const actual = await new TextraTranslator('test').request()
+        assert.deepStrictEqual(actual, res)
+      })
+    })
+
+    describe('リクエストが失敗した場合', () => {
+      describe('正常な API キーが取得できない場合', () => {
+        it('エラーを返却すること', () => {
+          apiKeyStub.returns('')
+
+          assert.rejects(async () => await new TextraTranslator('test').request(), EmptyVariableError)
+        })
+      })
+
+      describe('正常な API シークレットが取得できない場合', () => {
+        it('エラーを返却すること', () => {
+          apiSecretStub.returns('')
+
+          assert.rejects(async () => await new TextraTranslator('test').request(), EmptyVariableError)
+        })
+      })
+
+      describe('トークンが空の場合', () => {
+        it('エラーを返却すること', () => {
+          fetchTokenStub.rejects()
+
+          assert.rejects(async () => await new TextraTranslator('test').request(), EmptyVariableError)
+        })
+      })
+
+      describe('翻訳結果が空の場合', () => {
+        it('エラーを返却すること', () => {
+          fetchStub.rejects()
+
+          assert.rejects(async () => await new TextraTranslator('test').request(), EmptyVariableError)
+        })
+      })
+
+      describe('レスポンスが不正な場合', () => {
+        describe('レスポンスが null の場合', () => {
+          it('エラーを返却すること', () => {
+            const res = null
+            fetchStub.resolves(res)
+
+            assert.rejects(async () => await new TextraTranslator('test').request(), EmptyVariableError)
+          })
+        })
+
+        describe('レスポンス内にキーがない場合', () => {
+          it('エラーを返却すること', () => {
+            const res = {}
+            fetchStub.resolves(res)
+
+            assert.rejects(async () => await new TextraTranslator('test').request(), EmptyVariableError)
+          })
+        })
+
+        describe('レスポンス内の結果が空文字列の場合', () => {
+          it('エラーを返却すること', () => {
+            const res = { text: '' }
+            fetchStub.resolves(res)
+
+            assert.rejects(async () => await new TextraTranslator('test').request(), EmptyVariableError)
+          })
+        })
+      })
+    })
+  })
+})
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,22 @@
+export interface translatorResponse {
+  text: string
+}
+
 export interface gasResponse {
   text?: string
+}
+
+export interface tokenResponse {
+  access_token: string
+  expire_in: number
+  scope: string | null
+  token_type: string
+}
+
+export interface textraResponse {
+  resultset: {
+    result: {
+      text: string
+    }
+  }
 }
